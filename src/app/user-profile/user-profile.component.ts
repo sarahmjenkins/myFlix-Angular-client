3d4aeba82623316a4b6516a68a1d30154841bc2a
sarahmjenkins <sarahmjenkins88@gmail.com>
2022-11-30 17:31:31 -0700

    Added delete profile to component

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatDialogRef } from '@angular/material/dialog';
 import { MatDialog } from '@angular/material/dialog';
 import { EditUserComponent } from '../edit-user/edit-user.component';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-user-profile',
@@ -14,7 +17,10 @@ export class UserProfileComponent implements OnInit {
   
   constructor(
     public fetchApiData: FetchApiDataService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    public router: Router,
+    public snackBar: MatSnackBar,
+    public dialogRef: MatDialogRef<UserProfileComponent>,
   ) { }
 
   ngOnInit(): void {
@@ -30,5 +36,32 @@ export class UserProfileComponent implements OnInit {
 
   openEditUserDialog(): void {
    this.dialog.open(EditUserComponent)
+  }
+
+  // deleteAccount(): void {
+  //   if (confirm('Are you sure you want to delete your account?')) {
+  //     this.router.navigate(['welcome']).then(() => {
+  //       this.snackBar.open('Your account has been deleted', 'OK', { duration: 2000 });
+  //       this.dialogRef.close();
+  //     });
+      
+  //   }
+  //   this.fetchApiData.deleteUser().subscribe((result) => {
+  //     console.log(result);
+  //     localStorage.clear;
+  //   })
+  // }
+
+  deleteAccount(): void {
+    if (confirm('Are you sure you want to delete your account?')) {
+      this.router.navigate(['welcome']).then(() => {
+        this.snackBar.open('Your account has been deleted.', 'OK', {duration: 2000});
+        this.dialogRef.close();
+      });
+      this.fetchApiData.deleteUser().subscribe((result) => {
+        console.log(result);
+        localStorage.clear();
+      });
+    }
   }
 }
